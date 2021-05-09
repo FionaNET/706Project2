5291ac1daae23b61f1bd8bc61ee0b53559098cd0
Fiona <flin697@aucklanduni.ac.nz>
2021-05-10 09:29:24 +1200

    added unverified ultrasonic sensor files

---
+++ UltrasonicSensor.cpp
@@ -0,0 +1,63 @@
+#include <UltrasonicSensor.h>
+
+UltrasonicSensor::UltrasonicSensor(void)
+{
+    //empty constructor
+}
+
+float UltrasonicSensor::ReadUltraSonic(void)
+{
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 0 ) {
+    t2 = micros();
+    pulse_width = t2 - t1;
+    if ( pulse_width > (MAX_DIST + 1000)) {
+      SerialCom->println("HC-SR04: NOT found");
+      return MAX_DIST;
+    }
+  }
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1)
+  {
+    t2 = micros();
+    pulse_width = t2 - t1;
+    if ( pulse_width > (MAX_DIST + 1000) ) {
+      SerialCom->println("HC-SR04: Out of range");
+      return MAX_DIST;
+    }
+  }
+
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed
+  //of sound in air at sea level (~340 m/s).
+  cm = pulse_width / 58.0;
+
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+    SerialCom->println("HC-SR04: Out of range");
+    return MAX_DIST;
+  } else {
+    SerialCom->print("HC-SR04:");
+    SerialCom->print(cm);
+    SerialCom->println("cm");
+    return cm;
+  }
+}
