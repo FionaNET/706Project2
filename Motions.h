26188d536583b4e2a2a25275cfaa538726f42c05
vinulij01 <vinulij@outlook.com>
2021-05-11 12:18:09 +1200

    Changes to strafe and minor typo fixes

--- Motions.h
+++ Motions.h
@@ -4,14 +4,14 @@
 class Motions {
     public:
         void rotate_while_scan();
-
+        void obstical_avoid();
 
 
 
     private: 
         LightDetect* lightInfo = LightDetect();
-
-
+        float obsticalThresh;
+        bool backTracker;   //Tracks position of back wheels in regards to obsitcal
 
 
 
